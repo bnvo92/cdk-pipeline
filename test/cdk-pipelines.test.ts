abbd0c9dd2ca87ab61f4d8d2fe7bd2e78efb3e1a
brian <b.nvo92@gmail.com>
2021-07-16 03:51:08 -0400

    swapped to correct test stack

--- test/cdk-pipelines.test.ts
+++ test/cdk-pipelines.test.ts
@@ -1,12 +1,14 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CdkPipelines from '../lib/pipeline/cdk-pipelines-stack';
+import { ApplicationStack } from '../lib/application/application_stack'
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkPipelines.CdkPipelineStack(app, 'MyTestStack', {
+    const stack = new ApplicationStack(app, 'MyTestStack', {
       apiResourceName: 'test',
+      stageName: 'dev'
     });
     // THEN
     expectCDK(stack).to(matchTemplate({
